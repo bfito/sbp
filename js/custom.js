15f4bc80e94a05811ecaac065a0f3c3befe954e6
JPZ <jeanpaulzune@gmail.com>
2018-05-23 15:58:14 -0400

    Added autopadding to header

--- js/custom.js
+++ js/custom.js
@@ -76,3 +76,18 @@ $(document).ready(function(){
     });
   });
 });
+
+// Add autopadding to Header
+$(document).ready(function(){
+  'use strict';
+  setInterval(function(){
+    'use strict';
+    var windowHeight = $(window).height();
+    var containerHeight = $(".header-container").height();
+    var padTop = windowHeight - containerHeight;
+    $(".header-container").css({
+      'padding-top': Math.round(padTop / 2) + 'px',
+      'padding-bottom': Math.round(padTop / 2) + 'px'
+    });
+  }, 10)
+});

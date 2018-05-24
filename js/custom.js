8fa495889ef3db6f87f7c830ceab3ca1a1728cee
JPZ <jeanpaulzune@gmail.com>
2018-05-23 21:19:23 -0400

    Added slider

--- js/custom.js
+++ js/custom.js
@@ -91,3 +91,14 @@ $(document).ready(function(){
     });
   }, 10)
 });
+
+// Add bx slider to screens
+$(document).ready(function(){
+  $('.bxslider').bxSlider({
+    slideWidth: 292.5,
+    auto: true,
+    minSlides: 1,
+    maxSlides: 3,
+    slideMargin: 50
+  });
+});
